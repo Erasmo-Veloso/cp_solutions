077015b9ffccbbde5cb350fc5af9a2c6aa35685e
Erasmo-Veloso <creatorerasmo@gmail.com>
2025-11-13 23:37:06 +0100

    Solução para o problema: 1235 - Inside Out

--- Beecrowd/1235 - Inside Out.py
+++ Beecrowd/1235 - Inside Out.py
@@ -5,8 +5,8 @@ cases = int(input())
 for _ in range(cases):
     line = input()
     mid = len(line)//2
-    part_one = line[:mid]
-    part_two = line[(mid+1):]
+    part_one = line[:mid][:-1]
+    part_two = line[(mid+1):][:-1]
     
     result = ""
     
