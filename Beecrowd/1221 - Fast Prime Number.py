057890b6b46bed79aa81c780fea2af1b957d19e8
Erasmo-Veloso <creatorerasmo@gmail.com>
2025-11-14 17:54:51 +0100

    Solução para o problema: 1221 - Fast Prime Number

--- Beecrowd/1221 - Fast Prime Number.py
+++ Beecrowd/1221 - Fast Prime Number.py
@@ -5,6 +5,8 @@ for _ in range(N):
     c = 0
     isPrime = False
     to = int((100*len(str(number)))**(1/2))
+    if to < number:
+        c+=1
     for i in range(to):
         if c > 2:
             isPrime = True
