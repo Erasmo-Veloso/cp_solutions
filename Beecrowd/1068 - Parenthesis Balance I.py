8ebf4d39affe3511d48b86c7e4de7c7ba4059634
Erasmo-Veloso <creatorerasmo@gmail.com>
2025-11-15 15:29:39 +0100

    Solução para o problema: 1068 - Parenthesis Balance I

--- Beecrowd/1068 - Parenthesis Balance I.py
+++ Beecrowd/1068 - Parenthesis Balance I.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 
+import sys
+
 P = "()"
-
-expression = str(input())
-
-__p = ""
-isCorrect = True
-
-for element in expression:
-    if __p != element and element in P:
-        __p = element
-    elif __p == element and element in P:
-        isCorrect = False
-        break
-
-print("incorrect" if isCorrect else "is correct")
+for line in sys.stdin:
+    expression = str(line)
+    
+    __p = ""
+    isCorrect = True
+    
+    for element in expression:
+        if __p != element and element in P:
+            __p = element
+        elif __p == element and element in P:
+            isCorrect = False
+            break
+    
+    print("incorrect" if isCorrect else "is correct")
     
     
