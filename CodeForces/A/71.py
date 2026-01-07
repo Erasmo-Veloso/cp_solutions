b1c56ad37e6670428f3d570ffafa420dfbd183e5
Erasmo-Veloso <creatorerasmo@gmail.com>
2026-01-07 20:00:29 +0100

    Codeforces - 71A.py

---
+++ CodeForces/A/71.py
@@ -0,0 +1,14 @@
+#https://codeforces.com/problemset/problem/71/A
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    word = input().strip()
+    length = len(word)
+    if length > 10:
+        print(f'{word[0]}{length-2}{word[length-1]}')
+    else:
+        print(word)
